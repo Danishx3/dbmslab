6b5ca15fee1e72fedb0cd63b09d2cae8b1598e40
Danishx3 <danishkpmariyad@gmail.com>
2026-01-28 11:55:03 +0530

    updated page with buttons

--- part1/vite-project/src/App.jsx
+++ part1/vite-project/src/App.jsx
@@ -1,21 +1,25 @@
+import { useState } from 'react'
 import CourseInfo from './components/CourseInfo'
 import Unicafe from './components/Unicafe'
 import Anecdotes from './components/Anecdotes'
+import './App.css'
 
 const App = () => {
+  const [view, setView] = useState('courseinfo')
+
   return (
     <div>
-      <section>
-        <CourseInfo />
-      </section>
-      <hr />
-      <section>
-        <Unicafe />
-      </section>
-      <hr />
-      <section>
-        <Anecdotes />
-      </section>
+      <div className="navigation">
+        <button onClick={() => setView('courseinfo')}>Course Info</button>
+        <button onClick={() => setView('unicafe')}>Unicafe</button>
+        <button onClick={() => setView('anecdotes')}>Anecdotes</button>
+      </div>
+
+      <div className="content">
+        {view === 'courseinfo' && <CourseInfo />}
+        {view === 'unicafe' && <Unicafe />}
+        {view === 'anecdotes' && <Anecdotes />}
+      </div>
     </div>
   )
 }
